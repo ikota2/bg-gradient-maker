8c495a0fddde5160fa3d5b28d2ca6163301cea1d
Ivan <polyzay@gmail.com>
2021-02-22 18:00:13 +0300

    Updates

---
+++ precache-manifest.57f0d02e41de28c737e423df02c2a736.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e8a321c6c999f32c02f1465f0c5d107",
+    "url": "/bg-gradient-maker/index.html"
+  },
+  {
+    "revision": "ec554ec907469cff094b",
+    "url": "/bg-gradient-maker/static/css/main.5ddfd267.chunk.css"
+  },
+  {
+    "revision": "0cc840bd623ef3faca9a",
+    "url": "/bg-gradient-maker/static/js/2.8ef92d05.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bg-gradient-maker/static/js/2.8ef92d05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec554ec907469cff094b",
+    "url": "/bg-gradient-maker/static/js/main.b3770663.chunk.js"
+  },
+  {
+    "revision": "15fd7f3b4e97314e8dd7",
+    "url": "/bg-gradient-maker/static/js/runtime-main.ee951bc4.js"
+  }
+]);
